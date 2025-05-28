f46096e77609c127ebd227ae12542d0ccf9240f5
tonytrinh206 <tony.trinh@live.com.au>
2025-05-28 15:39:14 +1000

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -1,16 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { overrideGQLOperations } from '@dropins/build-tools/gql-extend.js';
+import { overrideGQLOperations } from "@dropins/build-tools/gql-extend.js";
 
 overrideGQLOperations([
   // ACCS does not have Downloadable Items
   {
-    npm: '@dropins/storefront-cart',
-    skipFragments: ['DOWNLOADABLE_CART_ITEMS_FRAGMENT'],
+    npm: "@dropins/storefront-cart",
+    skipFragments: ["DOWNLOADABLE_CART_ITEMS_FRAGMENT"],
     operations: [],
   },
   {
-    npm: '@dropins/storefront-order',
-    skipFragments: ['DOWNLOADABLE_ORDER_ITEMS_FRAGMENT'],
+    npm: "@dropins/storefront-order",
+    skipFragments: ["DOWNLOADABLE_ORDER_ITEMS_FRAGMENT"],
     operations: [],
+  },
+  {
+    npm: "@dropins/storefront-pdp",
+    operations: [
+      `
+      fragment PRODUCT_FRAGMENT on ProductView {
+        ... on SimpleProductView {
+            rating {
+              average
+              total
+            }
+        }
+      }
+    `,
+    ],
   },
 ]);
